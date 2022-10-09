5026178d23534c95723fd7356b1abe8294f25dc9
Tamutnih <renu07@list.ru>
2022-10-09 09:32:34 +0500

    решение

---
+++ task1.py
@@ -0,0 +1,22 @@
+# 1. Напишите программу, удаляющую из
+# текста все слова содержащие "абв".
+
+my_text = 'Напишите абв напиабв програбвмму программу, удаляющую из \
+    этого абв текста все вабвс слова, содерабващие содержащие "абв"'
+
+
+def del_some_words(my_text):
+    my_text = list(filter(lambda x: 'абв' not in x, my_text.split()))
+    return " ".join(my_text)
+
+
+my_text = del_some_words(my_text)
+print(my_text)
+
+# второе решение
+
+# txt = input("Введите текст через пробел:\n")
+# print(f"Исходный текст: {txt}")
+# find_txt = "абв"
+# lst = [i for i in txt.split() if find_txt not in i]
+# print(f'Результат: {" ".join(lst)}')
